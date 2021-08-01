67a49ca590131a0dce223c7c4e6614126d9c288f
itomblack <itomblack88@gmail.com>
2021-07-31 18:47:51 -0700

    try revealing variables

--- src/utils/ContentfulUtil.js
+++ src/utils/ContentfulUtil.js
@@ -2,8 +2,11 @@ import * as contentful from 'contentful'
 
 class ContentfulUtil {
   static client = contentful.createClient({
-    space: process.env.REACT_APP_SPACE_ID,
-    accessToken: process.env.REACT_APP_ACCESS_TOKEN
+    // space: process.env.REACT_APP_SPACE_ID,
+    // accessToken: process.env.REACT_APP_ACCESS_TOKEN
+    space:'pw1wgczsxbbk',
+    accessToken:'Aup43k9HRjzACAEq5nVJY2iffgM_TZAX9YXJUgtEjR8'
+
   })
   
   static fetchAllBlogPosts = () => ContentfulUtil.client.getEntries({
